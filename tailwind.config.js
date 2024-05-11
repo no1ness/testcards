153ad5cb3a02d8117725b2c3df9992cec9a92d59
no1ness <nooness@gmail.com>
2024-05-11 17:46:20 +0300

    Настроено адаптивное поведение картинок

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    screens: {
+      'ss': '320px',
+      'xs': '960px', // новая точка останова для экранов от 320 пикселей
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      // ... другие точки останова
+    },
     extend: {},
   },
   plugins: [],
